26f70030422266f0eb66d3a03559777d197c0998
dishant11max <savadiadishan@gmail.com>
2026-01-01 12:58:24 +0530

    Add initial Java files for basic programming concepts

---
+++ 2.variable.java
@@ -0,0 +1,15 @@
+class Var{
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        // float f1 = 5.54f;
+        // double d1=5.5;
+        // char c1 = 'k';
+        // boolean b1 = true;
+        // short s1=526;
+
+        int result = num1+num2;
+
+        System.out.println(result);
+    }
+}
---
+++ type.java
@@ -0,0 +1,21 @@
+public class type {
+    public static void main(String[] args) {
+        /* type casting
+         * 
+         * 
+         * int a = 258;
+         * byte b = (byte)a;
+         * 
+         * float f = 55.6f;
+         * int a = (int)f;
+         * 
+         * 
+         * 
+         */
+        // type promotion
+        byte b1=20;
+        byte b2=30;
+        int result = b1*b2;
+        System.out.println(result);
+    }
+}
