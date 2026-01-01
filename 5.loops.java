f7761af0dc7335e19730c6455ae337eadac68e2b
dishant11max <savadiadishan@gmail.com>
2026-01-01 14:23:51 +0530

    Add loops implementation with nested while loops

---
+++ 5.loops.java
@@ -0,0 +1,17 @@
+ class loops {
+    public static void main(String[] args) {
+        // java program implementing loops
+        // while loop
+        int i =1;
+        while (i<=5) {
+            System.out.println("hello "+i);
+            int j = 1;
+            while(j<=3){
+                System.out.println("hry "+j);
+                j++;
+            }
+            i++;
+        }
+        System.out.println("bye "+i);
+    }
+}
