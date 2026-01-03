f7a259cef106b70e4cbfe4abe0c324131b832f3d
dishant11max <savadiadishan@gmail.com>
2026-01-03 18:59:59 +0530

    use of this keyword

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,7 +1,23 @@
 class Desc{
+
+
    private String name = "Dishant";
    private int age = 22;
    private String device = "1+";
+
+    // constructor
+   public Desc(){
+    name="Devansh";
+    age=15;
+    device="Nokia";
+   }
+   
+   public Desc(int age, String name)  //parameterized constructor
+   {
+    this.age=age;
+    this.name=name;
+   }
+
 
    public String getName(){
     return name;
@@ -15,21 +31,28 @@ class Desc{
     return age;
    }
 
+
+
 //    setting the values
-   public void setAge(int a){
-    age=a;
+   public void setAge(int age){
+    this.age=age;
    }
-   public void setName(String s){
-    name=s;
+   public void setName(String name){
+    this.name=name;
    }
 }
 
 
 public class Encapsulation {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
        Desc obj = new Desc();
+       Desc obj1 = new Desc(18,"GreySon");
+        System.out.println(obj1.getName() + " : " +obj1.getAge()); 
+       System.out.println();
        obj.setAge(15);
        obj.setName("harsh");
        System.out.println(obj.getName() + " : " +obj.getAge()); 
     }
 }
+// this code also includes the use of this keyword along with constructors
