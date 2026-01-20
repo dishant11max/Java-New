2e8f709a074da3bd6f1df135a78ce778cd244a20
dishant11max <savadiadishan@gmail.com>
2026-01-20 21:08:57 +0530

    use of TreeSet

--- collectionApi.java
+++ collectionApi.java
@@ -2,27 +2,28 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class collectionApi {
     public static void main(String[] args) {
-    try {
-        Set<Integer> nums = new HashSet<Integer>();
-       nums.add(45);
-       nums.add(40);
-       nums.add(31);
-       nums.add(12);
-    //    nums.add("s")
-    //    System.out.println(nums.indexOf(4));
-    //    System.out.println("The index value of 2 is:"+nums.get(2));
-        for(int num : nums)
-            {
-            System.out.println(num);
+        try {
+            Set<Integer> nums = new TreeSet<Integer>();
+            nums.add(45);
+            nums.add(40);
+            nums.add(31);
+            nums.add(12);
+            // nums.add("s")
+            // System.out.println(nums.indexOf(4));
+            // System.out.println("The index value of 2 is:"+nums.get(2));
+            for (int num : nums) {
+                System.out.println(num);
             }
-        System.out.println(nums);
+            System.out.println(nums);
 
-    } catch (Exception e) {
-       System.out.println("Error Detected"+ e);
-    }
-       
+        } catch (Exception e) {
+            System.out.println("Error Detected" + e);
+        }
+
     }
 }
+// tree set sorts the array while hashset doesnt,hashset is used when you want unique values 
