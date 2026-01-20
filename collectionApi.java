0b78042fe21c41a75385e23b09c9a7362dd93d53
dishant11max <savadiadishan@gmail.com>
2026-01-20 21:22:56 +0530

    use of Map

--- collectionApi.java
+++ collectionApi.java
@@ -1,36 +1,17 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class collectionApi {
     public static void main(String[] args) {
-        try {
-            Collection<Integer> nums = new TreeSet<Integer>();
-            nums.add(45);
-            nums.add(40);
-            nums.add(31);
-            nums.add(12);
-            // nums.add("s")
-            // System.out.println(nums.indexOf(4));
-            // System.out.println("The index value of 2 is:"+nums.get(2));
-            Iterator<Integer> values = nums.iterator();
-
-            while (values.hasNext()) {
-                System.out.println(values.next());
-            }
-            // for (int num : nums) {
-            //     System.out.println(num);
-            // }
-            System.out.println(nums);
-
-        } catch (Exception e) {
-            System.out.println("Error Detected" + e);
+        Map<String, Integer> students = new HashMap<String, Integer>();
+        students.put("Dishy", 99);
+        students.put("Sahil", 32);
+        students.put("Subasis", 98);
+        students.put("Himashu", 49);
+         students.put("Sahil", 94);
+        for(String studs : students.keySet()){
+            System.out.println(studs + ":" + students.get(studs));
         }
-
+        System.out.println(students);
     }
 }
-// tree set sorts the array while hashset doesnt,hashset is used when you want unique values 
