b8c3dba0cfd9483a290e8925ccb0d709f685ed70
dishant11max <savadiadishan@gmail.com>
2026-01-20 21:05:15 +0530

    use of HashSet

--- collectionApi.java
+++ collectionApi.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class collectionApi {
     public static void main(String[] args) {
     try {
-        List<Integer> nums = new ArrayList<Integer>();
-       nums.add(5);
-       nums.add(4);
-       nums.add(3);
-       nums.add(2);
+        Set<Integer> nums = new HashSet<Integer>();
+       nums.add(45);
+       nums.add(40);
+       nums.add(31);
+       nums.add(12);
     //    nums.add("s")
-       System.out.println(nums.indexOf(4));
-       System.out.println("The index value of 2 is:"+nums.get(2));
+    //    System.out.println(nums.indexOf(4));
+    //    System.out.println("The index value of 2 is:"+nums.get(2));
         for(int num : nums)
             {
             System.out.println(num);
