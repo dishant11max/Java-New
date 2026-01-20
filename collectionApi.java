9c6e725d936ffd254bff781d3e702a539ac1c915
dishant11max <savadiadishan@gmail.com>
2026-01-20 21:44:37 +0530

    use of lambda expression with comparator

--- collectionApi.java
+++ collectionApi.java
@@ -7,16 +7,18 @@ public class collectionApi {
 
     public static void main(String[] args) {
 
-        Comparator<Integer> com = new Comparator<Integer>()
-         {
-            public int compare(Integer i, Integer j)
-             {
-                if (i % 10 > j % 10) {
-                    return 1;
-                } else
-                    return -1;
-            }
-        };
+        // Comparator<Integer> com = new Comparator<Integer>()
+        //  {
+        //     public int compare(Integer i, Integer j)
+        //      {
+        //         if (i % 10 > j % 10) {
+        //             return 1;
+        //         } else
+        //             return -1;
+        //     }
+        // };
+
+        Comparator<Integer> com = (i,j) -> i%10>j%10?1:-1;
         List<Integer> nums = new ArrayList<>();
         nums.add(25);
         nums.add(24);
